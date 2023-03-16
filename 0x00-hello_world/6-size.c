0c94b2b880c83f8d610eb2d493672d5a4a47807c
DKKowalski <sewunakpandana5@gmail.com>
2023-03-16 13:42:02 -0700

    6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -11,6 +11,15 @@ char a;
 long int b;
 long long int c;
 float d;
-printf("Size of a char: %zu\n Size of an int: %zu\n Size of a long int: %zu\n Size of a long long int: %zu\n Size of a float: %zu\n", sizeof(i), sizeof(a), sizeof(b), sizeof(c), sizeof(d));
+
+printf("Size of a char: %zu\n", sizeof(i));
+printf("Size of an int: %zu\n", sizeof(a));
+
+printf("Size of a long int: %zu\n", sizeof(b));
+
+printf("Size of a long long int: %zu\n", sizeof(c));
+
+printf("Size of a float: %zu\n", sizeof(d));
+
 return (0);
 }
