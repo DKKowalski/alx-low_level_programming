d86da243e519d72b6125307869511f40243fc4ed
DKKowalski <sewunakpandana5@gmail.com>
2023-03-21 13:03:08 -0700

    1-alphabet

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -5,12 +5,13 @@
  *
  * Return: void
  */
-void print_alpahabet(void)
+void print_alphabet(void)
 {
 char x_ter;
 for (x_ter = 'a'; x_ter <= 'z'; x_ter++)
+{
 _putchar(x_ter);
 
 _putchar('\n');
-
+}
 }
