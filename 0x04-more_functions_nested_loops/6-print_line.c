786d4e6ff2a85f052609dd2c461608692d6e22fa
DKKowalski <sewunakpandana5@gmail.com>
2023-03-23 11:50:22 -0700

    6-print_line

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * print_line - prints a line of n dashes, followed by a newline character
+ *
+ * @n: number of dashes to print
+ *
+ * Return: void
+ */
+void print_line(int n)
+{
+if (n > 0)
+{
+while (n > 0)
+{
+_putchar('-');
+n--;
+}
+}
+else
+{
+_putchar('\n');
+}
+
+_putchar('\n');
+}
+
