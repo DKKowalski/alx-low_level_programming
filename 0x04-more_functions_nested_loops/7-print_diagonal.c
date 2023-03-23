dd2e4c541456df76f3b20fb9d486b8f60da25d73
DKKowalski <sewunakpandana5@gmail.com>
2023-03-23 12:25:41 -0700

    7-diagonals

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,33 @@
+#include "main.h"
+/**
+ * print_diagonal - prints a diagonal line of '\' characters,
+ * followed by a newline character
+ *
+ * @n: length of the diagonal line to print
+ *
+ * Return: void
+ */
+void print_diagonal(int n)
+{
+int i, j;
+
+if (n > 0)
+{
+for (j = 0; j < n; j++)
+{
+for (i = 0; i < j; i++)
+{
+_putchar(' ');
+}
+_putchar('\\');
+_putchar('\n');
+}
+}
+else
+{
+_putchar('\n');
+}
+
+_putchar('\n');
+}
+
