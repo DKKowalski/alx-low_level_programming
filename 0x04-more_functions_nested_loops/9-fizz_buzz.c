6d60e1e8ee5b15a976b532f7182772204e337e8a
DKKowalski <sewunakpandana5@gmail.com>
2023-03-23 13:51:08 -0700

    changes to fizz_buzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -1,8 +1,10 @@
 #include <stdio.h>
 /**
- * fizzBuzz - Prints numbers from 1 to 100, replacing multiples of 3 with "Fizz",
+ * fizzBuzz - Prints numbers from 1 to 100,
+ * replacing multiples of 3 with "Fizz",
  *
- * multiples of 5 with "Buzz", and multiples of both 3 and 5 with "FizzBuzz".
+ * multiples of 5 with "Buzz",
+ * and multiples of both 3 and 5 with "FizzBuzz".
  *
  * Return: void
  */
