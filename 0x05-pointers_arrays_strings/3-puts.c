16e0c4c7a261f460d0558e71d6ee3bc4ea943955
DKKowalski <sewunakpandana5@gmail.com>
2023-03-27 06:39:02 -0700

    3-puts.c

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -1,3 +1,4 @@
+#include "main.h"
 #include <string.h>
 /**
  * _puts - prints a string to stdout
@@ -8,6 +9,6 @@ void _puts(char *str)
 {
 long unsigned int i;
 for (i = 0; i < strlen(str); i++) {
-putchar(str[i]);
+_putchar(str[i]);
 }
 }
