6f057b93cb9c17ab146b19ba79ac037f2289fcd5
DKKowalski <sewunakpandana5@gmail.com>
2023-03-27 04:57:54 -0700

    2-strlen.c

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,9 @@
+/**
+ * _strlen - Returns the length of a string.
+ * @s: The string to get the length of.
+ * Return: The length of the string.
+ */
+int _strlen(char *s)
+{
+return (strlen(s));
+}
