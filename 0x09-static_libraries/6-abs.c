13eec788341b0776bd753abdf71ac9104ff8979b
DKKowalski <sewunakpandana5@gmail.com>
2023-04-09 17:01:21 -0700

    libmy.a

---
+++ 0x09-static_libraries/6-abs.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _abs - computes the absolute value of an integer
+ * @c: integer to compute absolute value of
+ *
+ * Return: absolute value of integer
+ */
+int _abs(int c)
+{
+if (c < 0)
+{
+c = c * -1;
+}
+return (c);
+}
+
+
