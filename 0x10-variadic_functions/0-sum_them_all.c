cd5fb90ce24c69063e8363b424d18f1a0526a5d7
DKKowalski <sewunakpandana5@gmail.com>
2023-04-20 23:05:00 -0700

    0-sum_them_all.c

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -10,26 +10,19 @@
  * Return: The sum of the integers
  */
 int sum_them_all(const unsigned int n, ...)
-
 {
 va_list list;
 int i;
 int sum;
-	
 if (n == 0)
 {
 return (0);
 }
-
-
-
 va_start(list, n);
-
 for (i = 0; i < (int) n; i++)
 {
 sum += va_arg(list, int);
 }
 va_end(list);
-
 return (sum);
 }
