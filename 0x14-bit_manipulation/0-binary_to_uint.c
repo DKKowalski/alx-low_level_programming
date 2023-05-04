e53e783d24b86b6a40d7d4e6ace18188a43f9bf1
DKKowalski <sewunakpandana5@gmail.com>
2023-05-04 14:09:51 -0700

    bin int

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,5 +1,6 @@
 #include <stddef.h>
-#include <math.h>
+
+#include <string.h>
 /**
  * binary_to_uint - Convert a
  * binary number to an unsigned integer
@@ -8,11 +9,28 @@
  * Return: The converted unsigned integer,
  * or 0 if there is an error
  */
+ int power(unsigned int a, unsigned int b)
+ {
+    unsigned int pawa = 1;
+       if (b == 0)
+     {
+         return (pawa);
+     }
+     while(b != 0)
+     {
+         pawa = pawa * a;
+         --b;
+     }
+     
+     return (pawa);
+ }
 unsigned int binary_to_uint(const char *b)
 {
 unsigned int i;
 unsigned int decimal_num = 0;
 size_t len = strlen(b);
+if (b == NULL)
+	return (0);
 
 /* Check if input string is valid binary string */
 for (i = 0; i < len; i++)
@@ -26,7 +44,7 @@ return (0);  /* Return 0 to indicate error */
     /* Convert binary string to integer */
 for (i = 0; i < len; i++)
 {
-decimal_num += (b[i]-'0') * pow(2, len-i-1);
+decimal_num += (b[i]-'0') * power(2, len-i-1);
 }
 
 return (decimal_num);
