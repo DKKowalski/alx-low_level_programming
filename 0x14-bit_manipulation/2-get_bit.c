3205e73fe15846cac50bc2c576da2ea01b52e9e1
DKKowalski <sewunakpandana5@gmail.com>
2023-05-05 12:38:49 -0700

    2-get-bit

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -9,11 +9,12 @@
  * >= to the number of bits in an unsigned long int.
  */
 int get_bit(unsigned long int n, unsigned int index)
+
 {
+unsigned int  num_right_zeros = 1 << index;
 if (index >= sizeof(unsigned long int) * 8)
 	return (-1);
 
-unsigned long int  num_right_zeros = 1 << index;
 
 return ((n & num_right_zeros) != 0);
 }
